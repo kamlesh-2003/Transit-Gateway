7afa6ac4f65964b66fae52de0100e4bbc5a9516b
Kamlesh Kundkar <kundkarkamlesh@gmail.com>
2026-06-16 16:02:13 +0530

    vpc-2 output.tf modified

--- vpc-2/outputs.tf
+++ vpc-2/outputs.tf
@@ -1,9 +1,9 @@
 output "v2-vpc_id" {
-    value = aws_vpc.vpc-1.id
+    value = aws_vpc.vpc-2.id
 }
 output "v2-pub-subnet-id" {
-    value = aws_subnet.vpc-1-pub-subnet-1.id
+    value = aws_subnet.vpc-2-pub-subnet-1.id
 }
 output "v2-pri-subnet-id" {
-    value = aws_subnet.vpc-1-pri-subnet-1.id
+    value = aws_subnet.vpc-2-pri-subnet-1.id
 }
